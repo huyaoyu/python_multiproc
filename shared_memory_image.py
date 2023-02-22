e15b48f8343d09736a0638a54651c9c03fbeae3c
yaoyuh <yaoyuh@andrew.cmu.edu>
2023-02-22 11:59:43 -0500

    Update shared_memory_images.py. Change the documents and comments and the naming of internal variables.

--- shared_memory_image.py
+++ shared_memory_image.py
@@ -1,8 +1,8 @@
 from multiprocessing import shared_memory
 import numpy as np
 
-def shm_size_from_img_shape( img_shape, channel_depth, n_img ):
-    return int( np.prod( img_shape ) * channel_depth * n_img )
+def shm_size_from_img_shape( img_shape, channel_depth, n_grp=1 ):
+    return int( np.prod( img_shape ) * channel_depth * n_grp )
 
 def pass_forward(img):
     return img
@@ -50,67 +50,78 @@ def float_as_c4_uint8_arr(img):
     return img.view('<u1')
 
 class SharedMemoryImage(object):
-    def __init__(self, name: str, img_shape: tuple, channel_depth: int=1, grp_sz: int=1, n_img: int=20, processor_in=None, processor_out=None):
+    def __init__(self, name: str, img_shape: tuple, channel_depth: int=1, grp_sz: int=1, 
+                 n_grp: int=20, processor_in=None, processor_out=None):
         '''
         This is a wrapper class for using shared memory to store images across
         mulitple processes. The shared memory object is created by the user and
-        refered by the name.
+        refered internally by the name argument.
 
-        The user specifies the shape of the image. And the total number of images
-        in the shared memory is then determined by the size of the shared memory. 
+        The user specifies the shape (H, W, C) of the image, the channel depth (1 for uint8 type
+        and 4 for float type, etc), the group size, and the toal number of groups (n_grp). Then the 
+        total byte size of the shared memory is >= (H * W * C * channel_depth * grp_sz * n_grp), 
+        depdending on the architecture of the operating system.
         
         Note that the user is supposed to call initialize() and finalize() to properly
         handle the shared resources.
 
-        Note that internally, this class treats all image as uint8. The user must properly
+        Note that internally, this class treats all image as uint8, thus byte. The user must properly
         set the channel_depth value and the associated processor_in and processor_out functions. 
         porcessor_in is used to convert the input image to uint8 type with channel_depth. 
         Inversely, processor_out is used to convert the image from uint8 type to the original type.
-        Use None for processor_in and processor_out if no conversion is needed.
+        Use None for processor_in and processor_out if no conversions are needed.
 
         name: The name of the shared memory object.
-        img_shape: The shape of the image, including channel number.
+        img_shape: The shape of the image, [H, W, C], including channel number.
         channel_depth: The number of bytes per channel. Default is 1. For single-precision
             floating point values, use 4.
+        grp_sz: The number of images in a group. Default is 1.
+        n_grp: The total number of groups. Default is 20.
         processor_in: A callable object that performs the conversion to uint8.
         processor_out: A callable object that performs the conversion from uint8.
         '''
         super().__init__()
 
-        self.name = name
+        self.name = name # The name of the shared memory object.
         
         #print(f"SharedMemoryImage ~~ Group_Sz: {grp_sz}, Image_Shape: {img_shape}")
         if grp_sz != 1:
-            self.img_shape = (grp_sz, *img_shape[1:3], img_shape[3] * channel_depth) \
+            self.grouped_img_shape = (grp_sz, *img_shape[1:3], img_shape[3] * channel_depth) \
             if len(img_shape) == 4 else ( grp_sz, *img_shape[1:3], channel_depth )
         else:
-            self.img_shape = ( *img_shape[:2], img_shape[2] * channel_depth) \
+            self.grouped_img_shape = ( *img_shape[:2], img_shape[2] * channel_depth) \
             if len(img_shape) == 3 else ( *img_shape[:2], channel_depth )
-        #print(f"Resulting Img Shape: {self.img_shape}")
+        #print(f"Resulting Img Shape: {self.grouped_img_shape}")
 
-        self.img_capacity = np.prod( self.img_shape )
+        self.grp_capacity = np.prod( self.grouped_img_shape )
 
-        self.prorcessor_in = processor_in
-        self.prorcessor_out = processor_out
+        self.prorcessor_in  = processor_in if processor_in is not None else pass_forward
+        self.prorcessor_out = processor_out if processor_out is not None else pass_forward
 
         self.shm = None # User must call initialize() to assign a valid value.
-        self._n_img = n_img # This should not be assigned buy the user.
-
+        self.n_grp = n_grp
+        self.grp_sz = grp_sz
+        
     @property
     def n_img(self):
-        return self._n_img
+        return self.n_grp * self.grp_sz
+    
+    @property
+    def img_shape(self):
+        return self.grouped_img_shape[1:]
 
     def initialize(self):
         if ( self.shm is not None ):
             raise Exception('Already initialized. ')
 
-        print(f"Inside Initialization: {self.name}")
+        # print(f"Inside Initialization: {self.name}")
         self.shm = shared_memory.SharedMemory(name=self.name)
 
         # Check the size of the shared memory.
-        assert ( self.shm.size > 0 and self.shm.size // self.img_capacity == self.n_img ), \
-            'Incompatible shared memory size. self.shm.size = {}, self.img_capacity = {}, self.img_shape = {}'.format( 
-                self.shm.size, self.img_capacity, self.img_shape )
+        assert ( self.shm.size > 0 and self.shm.size // self.grp_capacity == self.n_grp ), \
+            ( f'Incompatible shared memory size. self.shm.size = {self.shm.size}, '
+              f'self.grp_capacity = {self.grp_capacity}, '
+              f'self.grouped_img_shape = {self.grouped_img_shape}. ' )
 
     def finalize(self):
         if ( self.shm is not None ):
@@ -120,30 +131,24 @@ class SharedMemoryImage(object):
         self.finalize()
 
     def seek(self, idx):
-        assert ( idx < self.n_img and idx >= 0 ), 'self.n_img = {}, idx = {}'.format( self.n_img, idx )
-        return self.shm.buf[ idx * self.img_capacity : (idx+1) * self.img_capacity ]
+        assert ( idx < self.n_grp and idx >= 0 ), f'self.n_grp = {self.n_grp}, idx = {idx}. '
+        return self.shm.buf[ idx * self.grp_capacity : (idx+1) * self.grp_capacity ]
 
     def __len__(self):
-        return self.n_img
+        return self.n_grp
 
     def as_array_at_idx(self, idx):
         return np.ndarray( 
-            ( self.img_capacity, ), 
+            ( self.grp_capacity, ), 
             dtype=np.uint8, 
             buffer=self.seek(idx) 
-            ).reshape( self.img_shape )
+            ).reshape( self.grouped_img_shape )
 
     def __getitem__(self, idx):
         array = self.as_array_at_idx(idx)
-
-        if self.prorcessor_out is not None:
-            return self.prorcessor_out(array)
-        else:
-            return array
+        return self.prorcessor_out(array)
 
     def __setitem__(self, idx, img):
-        if self.prorcessor_in is not None:
-            img = self.prorcessor_in(img)
-
+        img = self.prorcessor_in(img)
         array = self.as_array_at_idx(idx)
         array[:, :, ...] = img
